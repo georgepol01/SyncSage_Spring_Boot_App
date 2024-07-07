aaf7a3ec32933c8992c5bc44cf1afac0e391b2c5
georgepol01 <georgepol01@yahoo.com>
2024-07-07 18:21:21 +0300

    Initial commit

---
+++ src/main/java/com/syncsage/syncsage/SyncSageApplication.java
@@ -0,0 +1,13 @@
+package com.syncsage.syncsage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncSageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyncSageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/syncsage/syncsage/SyncSageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syncsage.syncsage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncSageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
